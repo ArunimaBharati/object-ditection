f2fe3191563e7709095aec465d3ee0ef03b2a0b3
ArunimaBharati <87299793+ArunimaBharati@users.noreply.github.com>
2022-05-31 18:50:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 img="";
+status="";
 
 function preload()
 {
@@ -9,6 +10,24 @@ function setup()
 {
     canvas=createCanvas(640,420);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status:detecting objects";
+}
+
+function modelLoaded()
+{
+    console.log("modelLoaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
 }
 
 function draw()
@@ -19,4 +38,10 @@ function draw()
     noFill();
     stroke("#FF0000");
     rect(30,60,450,350);
+
+    fill("#FF0000");
+    text("cat",320,120);
+    noFill();
+    stroke("FF0000");
+    rect(300,90,270,320);
 }
